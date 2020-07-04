71920313cfee2fb3698e38387914d4fb17bab6c0
Cyril-Obinna <Cyril-Obinna>
2020-07-04 10:53:08 +0200

    Automated unittest with python

--- Login.py
+++ Login.py
@@ -1,5 +1,8 @@
 import HtmlTestRunner
 from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.select import Select
 import time
 import unittest
 
@@ -10,29 +13,36 @@ class LoginTest(unittest.TestCase):
         cls.driver.implicitly_wait(10)
         cls.driver.maximize_window()
 
-    def test_login_valid(self): #Test function#
+    def test_login_valid(self):
         self.driver.get("https://opensource-demo.orangehrmlive.com")
         self.driver.find_element_by_id("txtUsername").send_keys("Admin")
+        time.sleep(1)
         self.driver.find_element_by_id("txtPassword").send_keys("admin123")
+        time.sleep(1)
         self.driver.find_element_by_xpath("//*[@class='button']").click()
-        self.driver.find_element_by_xpath("//*[@class='panelTrigger']").click()
-        self.driver.find_element_by_id("aboutDisplayLink").click()
-        self.driver.find_element_by_xpath("//*[@class='close']").click()
+        time.sleep(1)
+        self.driver.find_element_by_id("menu_admin_viewAdminModule").click()
+        time.sleep(1)
         self.driver.find_element_by_id("menu_pim_viewPimModule").click()
         time.sleep(1)
         self.driver.find_element_by_link_text("Dashboard").click()
         time.sleep(1)
         self.driver.find_element_by_id("welcome").click()
         time.sleep(1)
+        self.driver.find_element_by_id("aboutDisplayLink").click()
+        time.sleep(1)
+        self.driver.find_element_by_xpath("//*[@class='close']").click()
         self.driver.find_element_by_link_text("Logout").click()
         time.sleep(1)
-    def test_logout(self): #Test function#
-        self.driver.close()
-        self.driver.quit()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.close()
+        cls.driver.quit()
         print("Test Completed")
 
 if __name__ == '__main__':
     unittest.main()
 
-#To Run on Terminal = python -m Login#
+#To Run on Terminal = python Login.py#
 
